6b4203de466c95dfff58494ba16ed27771829f35
biqoffjay <destinyadah673@gmail.com>
2024-11-19 00:36:51 +0100

    Add files via upload

---
+++ destiny.py
@@ -0,0 +1,29 @@
+def a():
+    print("Addition")
+    num1 =float(input("input your first digit:"))
+    num2 =float(input("input your second digit:"))
+    print(num1+num2)
+
+def b():
+    print("Subtraction")
+    num1 =float(input("input your first digit:"))
+    num2 =float(input("input your second digit:"))
+    print(num1-num2)
+
+def c():
+    print("Multiplication")
+    num1 =float(input("input your first digit:"))
+    num2 =float(input("input your second digit:"))
+    print(num1*num2)
+
+def d():
+    print("Divivsion")
+    num1 =float(input("input your first digit:"))
+    num2 =float(input("input your second digit:"))
+    print(num1/num2)
+
+def b():
+    print("RIased to the Power of")
+    num1 =float(input("input your first digit:"))
+    num2 =float(input("input your second digit:"))
+    print(num1^num2)
